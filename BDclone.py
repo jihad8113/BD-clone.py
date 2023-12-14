92821b21fd659dc9ca5d5969d402a70a26c1cd72
jihad8113 <138552167+jihad8113@users.noreply.github.com>
2023-12-14 17:12:50 +0600

    Update BDclone.py

--- BDclone.py
+++ BDclone.py
@@ -231,7 +231,7 @@ def action():
     '[\xe2\x9c\x93] Total OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
     print '[\xe2\x9c\x93] CP File Has Been Saved : save/checkpoint.txt'
     raw_input('\n[Press Enter To Go Back]')
-    os.system('python2 jihad.py')
+    os.system('python2 BDclone.py')
 
 
 if __name__ == '__main__':
